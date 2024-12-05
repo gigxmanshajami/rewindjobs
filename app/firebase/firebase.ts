f6594c9279f848094afdeaddb573712892c5a1bf
gigxmanshajami <mansajami2020@gmail.com>
2024-12-05 13:25:50 +0530

    updated

--- app/firebase/firebase.ts
+++ app/firebase/firebase.ts
@@ -3,17 +3,18 @@ import { getFirestore, collection, getDocs } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage"; // Import Firebase Storage
 
-// Your web app's Firebase configuration
+//  web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC7i_t4rS2FvR9nhdT8_IufVK2Xy4rvx8c",
-  authDomain: "jobtracker-3b3a9.firebaseapp.com",
-  projectId: "jobtracker-3b3a9",
-  storageBucket: "jobtracker-3b3a9.appspot.com",
-  messagingSenderId: "469255399942",
-  appId: "1:469255399942:web:eab06b2ccb14f20558b187",
-  measurementId: "G-M1BHZ6QY55",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
-// Initialize Firebase
+
+// Initializing Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app); // Initialize Firebase Storage
